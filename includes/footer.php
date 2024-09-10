68b4f43866f34773d13f52b6c1ced244f06d2dad
hammadalimughal <hammad19ali@gmail.com>
2024-09-10 14:35:38 +0500

    project uploading

---
+++ includes/footer.php
@@ -0,0 +1,111 @@
+<footer class="footer-main">
+    <div class="container-fluid ppx-55">
+        <div class="row">
+            <div class="col-lg-2">
+                <div class="foo-column foo1">
+                    <a href="#">
+                        <img src="images/footer_logo.png" class="img-fluid" alt="Footer Logo">
+                    </a>
+                    <p>Putting the Her in Traveler <span class="bold-font">the perfect bag for every itinerary</span></p>
+                </div>
+            </div>
+            <div class="col-lg-1"></div>
+            <div class="col-lg-2">
+                <div class="foo-column foo2">
+                    <h3>Policies</h3>
+                    <ul>
+                        <li><a href="#">Shipping Policy</a></li>
+                        <li><a href="#">Return/Refund Policy</a></li>
+                        <li><a href="#">Privacy Policy</a></li>
+                        <li><a href="#">Terms of Service</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-lg-2">
+                <div class="foo-column foo3">
+                    <h3>Support</h3>
+                    <ul>
+                        <li><a href="#">Contact Us</a></li>
+                        <li><a href="#">Track Order</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-lg-2">
+                <div class="foo-column foo4">
+                    <h3>Contact Us</h3>
+                    <ul>
+                        <li><a href="mailto:info@travelherbag.com">info@travelherbag.com</a></li>
+                        <li><a href="javascript:;">Dallas, Texas</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-lg-3">
+                <div class="foo-column foo5">
+                    <h2>Subscribe to our <br /><span class="bold-font">Emails</span></h2>
+                    <form action="#" method="Post">
+                        <input type="text" name="text" placeholder="Enter Your Email Id">
+                        <button type="button" class="btn btn-default">Sign Up</button>
+                    </form>
+
+                </div>
+            </div>
+        </div>
+        <div class="bottom-footer">
+            <hr />
+            <div class="row">
+                <div class="col-lg-6 col-md-6 col-6">
+                    <p class="developer-copy">© 2024 TravelHer</p>
+                </div>
+                <div class="col-lg-6 col-md-6 col-6">
+                    <ul class="social-media">
+                        <li>
+                            <a href="#">
+                                <svg width="13" height="23" viewBox="0 0 13 23" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M8.39101 22.6456V12.893H11.636L12.2529 8.85868H8.39101V6.24082C8.39101 5.13713 8.93025 4.06135 10.6601 4.06135H12.4163V0.626925C12.4163 0.626925 10.8227 0.354492 9.29951 0.354492C6.11903 0.354492 4.03973 2.28649 4.03973 5.78407V8.85868H0.503906V12.893H4.03973V22.6456H8.39101Z" fill="white" />
+                                </svg>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <svg width="17" height="17" viewBox="0 0 17 17" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M9.94078 7.27137L15.7317 0.524414H14.3595L9.3312 6.38284L5.31548 0.524414H0.683594L6.75663 9.38328L0.683594 16.4585H2.05587L7.3662 10.2718L11.6076 16.4585H16.2395L9.94078 7.27137Z" fill="white" />
+                                </svg>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                            <svg width="19" height="19" viewBox="0 0 19 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M14.2723 3.64551C13.684 3.64551 13.207 4.12355 13.207 4.71321C13.207 5.30287 13.684 5.78092 14.2723 5.78092C14.8606 5.78092 15.3376 5.30287 15.3376 4.71321C15.3376 4.12355 14.8606 3.64551 14.2723 3.64551Z" fill="white" stroke="white" stroke-width="0.5" stroke-miterlimit="10"/>
+<path d="M9.53759 5.01465C7.06998 5.01465 5.0625 7.02669 5.0625 9.49989C5.0625 11.9731 7.06998 13.9851 9.53759 13.9851C12.0052 13.9851 14.0127 11.9731 14.0127 9.49989C14.0127 7.02669 12.0052 5.01465 9.53759 5.01465ZM9.53759 12.3733C7.9565 12.3733 6.67069 11.0846 6.67069 9.49989C6.67069 7.91522 7.9565 6.62649 9.53759 6.62649C11.1187 6.62649 12.4045 7.91522 12.4045 9.49989C12.4045 11.0846 11.1187 12.3733 9.53759 12.3733Z" fill="white" stroke="white" stroke-width="0.5" stroke-miterlimit="10"/>
+<path d="M13.0889 18.607H5.83334C2.82356 18.607 0.375 16.1528 0.375 13.1362V5.86425C0.375 2.84766 2.82356 0.393555 5.83334 0.393555H13.0889C16.0987 0.393555 18.5479 2.84766 18.5479 5.86425V13.1362C18.5479 16.1528 16.0987 18.607 13.0889 18.607ZM5.83334 2.10673C3.76577 2.10673 2.08431 3.792 2.08431 5.86425V13.1362C2.08431 15.2085 3.76577 16.8938 5.83334 16.8938H13.0889C15.1565 16.8938 16.8379 15.2085 16.8379 13.1362V5.86425C16.8379 3.792 15.1557 2.10673 13.0889 2.10673H5.83334Z" fill="white" stroke="white" stroke-width="0.5" stroke-miterlimit="10"/>
+</svg>
+
+
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <svg width="15" height="16" viewBox="0 0 15 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M0.464675 5.40183H3.46639V15.067H0.464675V5.40183ZM1.96663 0.598633C2.92642 0.598633 3.7045 1.37922 3.7045 2.33898C3.7045 3.29874 2.92642 4.08226 1.96663 4.08226C1.00685 4.08226 0.226562 3.3002 0.226562 2.33898C0.226562 1.37775 1.00465 0.598633 1.96663 0.598633Z" fill="white" />
+                                    <path d="M5.34766 5.4015H8.22262V6.72328H8.26218C8.66295 5.96252 9.64105 5.16211 11.1005 5.16211C14.1352 5.16211 14.695 7.1624 14.695 9.76558V15.0666H11.6998V10.3677C11.6998 9.24568 11.6778 7.80494 10.1422 7.80494C8.58162 7.80494 8.34424 9.02611 8.34424 10.2862V15.0666H5.34838L5.34766 5.4015Z" fill="white" />
+                                </svg>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+</main>
+<script src="js/jquery-3.6.3.min.js"></script>
+<script src="js/bootstrap.bundle.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.js" integrity="sha512-XtmMtDEcNz2j7ekrtHvOVR4iwwaD6o/FUJe6+Zq+HgcCsk3kj4uSQQR8weQ2QVj1o0Pk6PwYLohm206ZzNfubg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<script src="js/owl.carousel.min.js"></script>
+<script src="js/gsap/gsap.min.js"></script>
+<script src="js/gsap/ScrollTrigger.min.js"></script>
+<script src="js/gsap/ScrollSmoother.min.js"></script>
+<script src="js/custom.js"></script>
+</body>
+
+</html>
